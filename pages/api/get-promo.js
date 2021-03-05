4ac0333252d189d0994698e4f0c7b29973051b69
Vinícius Marque Zaninelo <vinicousmarqueszaninelo@gmail.com>
2021-03-04 21:42:16 -0300

    fix

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,20 +1,13 @@
-
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-
+import { fromBase64 } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-const fromBase64 = value => {
-  const buff = new Buffer(value, 'base64');
-  return buff.toString('ascii');
-}
-
 export default async (req, res) => {
-  console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: (fromBase64(process.env.SHEET_PRIVATE_KEY))
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     })
     await doc.loadInfo()
 
